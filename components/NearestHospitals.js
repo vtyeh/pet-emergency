b801a6f8ea14bd6624a15d5cbc3a0a87cda70642
Venessa  Yeh <venessayeh@Venessas-MacBook-Air.local>
2021-08-12 21:39:19 -0700

    fix next/image import statement

--- components/NearestHospitals.js
+++ components/NearestHospitals.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Image from "next/Image";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import planet from "../public/location-request.svg";
 
