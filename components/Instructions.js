5decd02d29d6e63693fd259bc547fa08135c48e1
Venessa  Yeh <venessayeh@Venessas-MacBook-Air.local>
2021-08-12 22:46:37 -0700

    add filler image for cpr instructions, fix mobile styling

--- components/Instructions.js
+++ components/Instructions.js
@@ -1,9 +1,12 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import cat from "../public/cat.svg";
 
 export default function Instructions() {
   return (
     <section className={styles.instructions}>
-      <h3>Choking and CPR</h3>
+      {/* <h3>Choking and CPR</h3> */}
+      <Image src={cat} alt="cat" />
     </section>
   );
 }
