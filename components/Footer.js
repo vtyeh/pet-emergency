d54fba31d98628e69a293683f73226fe13ab864e
vtyeh <vtyeh24@gmail.com>
2021-08-17 15:12:06 -0700

    add last edited date to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,12 @@
 import styles from "../styles/Home.module.css";
 
 export default function Footer() {
+  // TODO: update this before deploying
+  const lastEditDate = "8-17-2021";
   return (
     <footer className={styles.footer}>
+      Last edited on {lastEditDate}.
+      <br />
       Created with and by LOVE. Powered by Taiwanese tea. Copyright © 2021.
       <br />
       In loving memory of Kimchi 🧡
