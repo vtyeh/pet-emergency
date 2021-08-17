bf27fe387811e2a2065957bc4dcf64b5087894de
Venessa Yeh <vtyeh24@gmail.com>
2021-08-16 19:07:39 -0700

    fix navbar structure

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Created with and by LOVE. Powered by Taiwanese tea. Copyright © 2021.
+      <br />
+      In loving memory of Kimchi 🧡
+    </footer>
+  );
+}
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,32 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/logo.svg";
 
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
-      <Link href="/">
-        <a>PetEmergency</a>
-      </Link>
-      <div className={styles.navbarLinks}>about us</div>
+      <div className={styles.company}>
+        <div className={styles.logo}>
+          <Image
+            // className={styles.logo}
+            src={logo}
+            alt="logo"
+            width="20"
+            height="20"
+          />
+        </div>
+        <div className={styles.name}></div>
+        <Link href="/">
+          <a>PetEmergency</a>
+        </Link>
+      </div>
+
+      <div className={styles.navbarLinks}>
+        <Link href="/about">
+          <a>about us</a>
+        </Link>
+      </div>
     </div>
   );
 }
