a2d078b35439df6b7626e3d9ffae299b08a2ccaf
vtyeh <vtyeh24@gmail.com>
2021-08-19 22:54:37 -0700

    use original gif for now

--- components/Instructions.js
+++ components/Instructions.js
@@ -1,24 +1,18 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
-import instructionGif from '../public/cpr-instructions/gif-test.gif';
+// import instructionGif from '../public/cpr-instructions/cpr-1.gif';
+import instructionGif from '../public/cpr-instructions/cpr-original.gif';
 
 export default function Instructions() {
   return (
     <section className={styles.instructions}>
       <h3>Choking and CPR</h3>
-      {/* <Image src={instructionGif} alt="cat" className={styles.instructionGif} /> */}
       <div className={styles.instructionCard}>
         <Image
           src={instructionGif}
           alt="cat"
           className={styles.instructionGif}
         />
-
-        {/* <img
-          src="/cpr-instructions/CPR-Cats-and-Dogs-1.gif"
-          alt="instruction gif"
-          className={styles.instructionGif}
-        /> */}
       </div>
     </section>
   );
