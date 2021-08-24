28f3fb38b0c1c07c5463a298bb54b6545c71758a
vtyeh <vtyeh24@gmail.com>
2021-08-23 17:33:57 -0700

    make infographic responsive for mobile

--- components/Instructions.js
+++ components/Instructions.js
@@ -6,7 +6,7 @@ import instructionGif from '../public/cpr-instructions/cpr-original.gif';
 export default function Instructions() {
   return (
     <section className={styles.instructions}>
-      <h3>Choking and CPR</h3>
+      <h3 style={{ width: '200px' }}>Choking and CPR</h3>
       <div className={styles.instructionCard}>
         <Image
           src={instructionGif}
